d13614ff5c73c9e8a0abd5fc74e436131343a2f6
lalit1794 <lalit1794@gmail.com>
2016-04-20 18:09:22 +0530

    cron job written

--- app/controllers/scrape_controller.rb
+++ app/controllers/scrape_controller.rb
@@ -1,9 +1,9 @@
 class ScrapeController < ApplicationController
-	def scrape
+	def scrape	
 		require 'nokogiri'
-		require 'open-uri'
-		url = "http://www.ietlucknow.edu/result.htm"
-		data = Nokogiri::HTML(open(url))
-		@links = data.css('#inner_text_1 a')
+    require 'open-uri'
+    url = "http://www.ietlucknow.edu/result.htm"
+    data = Nokogiri::HTML(open(url))
+    @links = data.css('#inner_text_1 a')
 	end	
 end
