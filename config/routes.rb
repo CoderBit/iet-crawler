12e89bd514ff353a08a647dc1f201db81da3cbfa
lalit1794 <lalit1794@gmail.com>
2016-05-02 17:30:39 +0530

    do the ui after this commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
 
   # Serve websocket cable requests in-process
   # mount ActionCable.server => '/cable'
-  root :to => 'users#index'
+  root :to => 'users#new'
 
 end
